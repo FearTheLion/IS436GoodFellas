5ee7ed5b6d33d675d662d7b849b8063ac4486a53
tyatsko71 <tyatsko1@umbc.edu>
2019-03-13 21:43:02 -0400

    Django Framework Installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='IS436GoodFellas',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='tyatsko',
+    author_email='',
+    description=''
+)
